3bcad91eea8f263ff5d40019885edf0b10e58d3f
akash jha <akashjhasciencea@gmail.com>
2025-04-15 08:37:44 +0530

    Removing empty lines

--- src/app/mindmap/page.tsx
+++ src/app/mindmap/page.tsx
@@ -12,21 +12,19 @@ import PricingPortal from "@/components/PricingPortal";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar"
 import { TaskadeSidebar } from "@/components/TaskadeSidebar"
-import {useNextStep } from "nextstepjs"
+import { useNextStep } from "nextstepjs"
 import { HtmlContentProvider, useHtmlContentContext } from "@/contexts/HTMLContextProvider";
 import { useMindmapState } from "@/contexts/MindmapStateProvider";
 import { useTaskContext } from "@/contexts/TaskProvider";
 import { useSaveHtmlContent, useMindmapActions } from "@/hooks/all-hooks";
-
 export default function Home() {
   const { data: session } = useSession();
   const editorRef = useRef<EditorView | null>(null);
   const { isVerified, setIsVerified, loading, inputValue, setInputValue, saving, mode, setMode, showPricing, setShowPricing, error } = useMindmapState();
-  const {  currentLoadingStep, loadingMessages, messageIndex } = useTaskContext();
+  const { currentLoadingStep, loadingMessages, messageIndex } = useTaskContext();
   const { htmlContent, setHtmlContent } = useHtmlContentContext();
   const { loadSavedMindmap } = useSaveHtmlContent(editorRef);
   const { handleSave, handleSubmitWebhook, enterFullscreen, fixSyntax } = useMindmapActions(editorRef);
-
   useEffect(() => {
     const hasSeenTutorial = localStorage.getItem('hasSeenTutorial') === 'true';
     if (!hasSeenTutorial) {
@@ -39,10 +37,6 @@ export default function Home() {
       loadSavedMindmap(mindmapId);
     }
   }, []);
-
-
-
-
   // Use the tour functionality
   const { startNextStep } = useNextStep();
   const handleStartTour = () => {
@@ -50,16 +44,14 @@ export default function Home() {
   };
   return (
     <>
-
-        <div className="flex h-screen relative">
+      <div className="flex h-screen relative">
         <PricingPortal isOpen={showPricing} />
         {session && (
           <SidebarProvider>
-            <AppSidebar/>
+            <AppSidebar />
             <SidebarTrigger />
           </SidebarProvider>
         )}
-        
         <div className="flex-1 flex flex-col items-center pb-[80px]"> {/* Added padding-bottom for footer */}
           {/* Top section: Input and Controls */}
           <div className="w-full flex flex-col items-center justify-start pt-8 pb-4">
@@ -98,7 +90,6 @@ export default function Home() {
                         } else {
                           setIsVerified(true);
                         }
-
                       } catch (error) {
                         console.error("Error updating usage count:", error);
                         setIsVerified(true);
@@ -109,18 +100,16 @@ export default function Home() {
               </div>
             ) : (
               <>
-                  <Input
+                <Input
                   id="input"
-                    placeholder="Mindmap content"
-                    value={inputValue}
-                    onChange={(e) => setInputValue(e.target.value)}
-                    className="pl-2 pr-2 w-1/2 justify-center mb-6"
-                  />
-              
-                  <Button variant="default" onClick={handleSubmitWebhook} disabled={loading}>
-                    {loading? <Loader2 className="animate-spin w-4 h-4" /> : 'Generate Mindmap'}
-                  </Button>
-             
+                  placeholder="Mindmap content"
+                  value={inputValue}
+                  onChange={(e) => setInputValue(e.target.value)}
+                  className="pl-2 pr-2 w-1/2 justify-center mb-6"
+                />
+                <Button variant="default" onClick={handleSubmitWebhook} disabled={loading}>
+                  {loading ? <Loader2 className="animate-spin w-4 h-4" /> : 'Generate Mindmap'}
+                </Button>
                 {error ? <p className="text-red-600">{error}</p> : null}
               </>
             )}
@@ -145,35 +134,24 @@ export default function Home() {
           </div>
 
           {/* Middle section: Mindmap Iframe */}
-         <HtmlContentProvider>
-         <MindmapEditor editorRef={editorRef} htmlContent={htmlContent} setHtmlContent={setHtmlContent}/>
-         </HtmlContentProvider>
-            
-         
-            
+          <HtmlContentProvider>
+            <MindmapEditor editorRef={editorRef} htmlContent={htmlContent} setHtmlContent={setHtmlContent} />
+          </HtmlContentProvider>
           {/* Bottom section: Buttons and Editor */}
           <div className="w-full flex flex-col items-center">
             <div className="flex space-x-2 mb-4 mt-10" id="buttons">
-            <Button variant="outline" onClick={handleSave} disabled={saving}>
-              {saving ? <Loader2 className="animate-spin w-4 h-4" /> : 'Save Changes'}
-            </Button>
-            <Button variant="outline" onClick={enterFullscreen}>Go Fullscreen</Button>
-            <Button variant="outline" onClick={fixSyntax}>Fix Syntax</Button>
-            <Button variant="outline" onClick={handleStartTour}>Show tour</Button>
+              <Button variant="outline" onClick={handleSave} disabled={saving}>
+                {saving ? <Loader2 className="animate-spin w-4 h-4" /> : 'Save Changes'}
+              </Button>
+              <Button variant="outline" onClick={enterFullscreen}>Go Fullscreen</Button>
+              <Button variant="outline" onClick={fixSyntax}>Fix Syntax</Button>
+              <Button variant="outline" onClick={handleStartTour}>Show tour</Button>
+            </div>
           </div>
-
-
-          </div>
-          
         </div>
-        <TaskadeSidebar/>
+        <TaskadeSidebar />
       </div>
-
-
-     
-      
     </>
   );
 }
-
 export const dynamic = 'force-dynamic';
